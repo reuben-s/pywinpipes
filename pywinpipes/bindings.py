6733dfbb1e599956590961da5c87d313820f474f
tenways <rdasmith01@icloud.com>
2023-10-09 20:15:33 +0100

    Complete bindings

--- pywinpipes/bindings.py
+++ pywinpipes/bindings.py
@@ -16,13 +16,13 @@ from ctypes import (
 
 # Windows data type definitions
 
-HANDLE  = c_void_p       # HANDLE is a pointer ((I think I need to implement the handle type properly))
-
+HANDLE  = c_void_p       # A handle to an object.
 DWORD   = c_ulong        # A 32-bit unsigned integer.
 LPDWORD = POINTER(DWORD) # Pointer to a DWORD type.
 LPCWSTR = c_wchar_p      # A pointer to a constant null-terminated string of 16-bit Unicode characters.
 BOOL    = c_bool         # A Boolean variable (should be TRUE or FALSE).
 LPVOID  = c_void_p       # A pointer to any type.
+LPCVOID = c_void_p       # A pointer to a constant of any type.
 
 class SECURITY_ATTRIBUTES(Structure):
     _fields_ = [
@@ -31,18 +31,37 @@ class SECURITY_ATTRIBUTES(Structure):
         ("bInheritHandle", BOOL)
     ]
 
+LPSECURITY_ATTRIBUTES = POINTER(SECURITY_ATTRIBUTES)
 
 # Load DLL
 
 libname = Path().absolute() / "pywinpipes/bindings/x64/Debug/bindings.dll"
 cdll = CDLL(str(libname))
 
-
 # Define bindings for C++ functions
 
 # CreateNamedPipe()
-cdll.bCreateNamedPipe.restype = HANDLE
-def CreateNamedPipe(lpName, dwOpenMode, dwPipeMode, nMaxInstances, nOutBufferSize, nInBufferSize, nDefaultTimeOut, lpSecurityAttributes) -> BOOL:
+cdll.bCreateNamedPipe.argtypes = [
+    LPCWSTR,
+    DWORD,
+    DWORD,
+    DWORD,
+    DWORD,
+    DWORD,
+    DWORD,
+    LPSECURITY_ATTRIBUTES
+]
+cdll.bCreateNamedPipe.restype  = HANDLE
+def CreateNamedPipe(
+    lpName, 
+    dwOpenMode, 
+    dwPipeMode, 
+    nMaxInstances, 
+    nOutBufferSize, 
+    nInBufferSize, 
+    nDefaultTimeOut, 
+    lpSecurityAttributes
+    ):
     return cdll.bCreateNamedPipe(
         LPCWSTR(lpName), 
         DWORD(dwOpenMode), 
@@ -55,15 +74,67 @@ def CreateNamedPipe(lpName, dwOpenMode, dwPipeMode, nMaxInstances, nOutBufferSiz
     )
 
 # ConnectNamedPipe()
-cdll.bConnectNamedPipe.restype = BOOL
+cdll.bConnectNamedPipe.argtypes = [ HANDLE ]
+cdll.bConnectNamedPipe.restype  = BOOL
 def ConnectNamedPipe(hNamedPipe):
-    return cdll.bConnectNamedPipe(
-        HANDLE(hNamedPipe)
-    )
+    return cdll.bConnectNamedPipe(HANDLE(hNamedPipe))
 
 # DisconnectNamedPipe()
-cdll.bDisconnectNamedPipe.restype = BOOL
+cdll.bDisconnectNamedPipe.argtypes = [ HANDLE ]
+cdll.bDisconnectNamedPipe.restype  = BOOL
 def DisconnectNamedPipe(hNamedPipe):
-    return cdll.bDisconnectNamedPipe(
-        HANDLE(hNamedPipe)
+    return cdll.bDisconnectNamedPipe(HANDLE(hNamedPipe))
+
+# ReadFile()
+cdll.bReadFile.argtypes = [
+    HANDLE,
+    LPVOID,
+    DWORD,
+    LPDWORD
+]
+cdll.bReadFile.restype  = BOOL
+def ReadFile(
+    hFile, 
+    lpBuffer, 
+    nNumberOfBytesToRead, 
+    lpNumberOfBytesRead
+    ):
+    return cdll.bReadFile(
+        HANDLE(hFile),
+        LPVOID(lpBuffer),
+        DWORD(nNumberOfBytesToRead),
+        LPDWORD(lpNumberOfBytesRead)
     )
+
+# WriteFile()
+cdll.bWriteFile.argtypes = [
+    HANDLE,
+    LPCVOID,
+    DWORD,
+    LPDWORD
+]
+cdll.bWriteFile.restype  = BOOL
+def WriteFile(
+    hFile, 
+    lpBuffer, 
+    nNumberOfBytesToWrite, 
+    lpNumberOfBytesWritten
+    ):
+    return cdll.bWriteFile(
+        HANDLE(hFile),
+        LPCVOID(lpBuffer),
+        DWORD(nNumberOfBytesToWrite),
+        LPDWORD(lpNumberOfBytesWritten)
+    )
+
+# CloseHandle()
+cdll.bCloseHandle.argtypes = [ HANDLE ]
+cdll.bCloseHandle.restype  = BOOL
+def CloseHandle(hObject):
+    return cdll.bCloseHandle(HANDLE(hFile))
+
+# FlushFileBuffers()
+cdll.bFlushFileBuffers.argtypes = [ HANDLE ]
+cdll.bFlushFileBuffers.restype  = BOOL
+def FlushFileBuffers(hFile):
+    return cdll.bFlushFileBuffers(HANDLE(hFile))
